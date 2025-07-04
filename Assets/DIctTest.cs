433cd00f2faa158d2e9241a9f73d1ac39169dc50
Heoyoon <parechae@gmail.com>
2025-07-04 22:25:16 +0900

    feat : 프로젝트 세팅

---
+++ Assets/DIctTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIctTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+
+    }
+}
